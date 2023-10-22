6bc914c7fccc3424802e283b91c8c6c9dd9c5e69
imminseohoe <imminseohoe@gmail.com>
2023-10-22 15:50:00 +0700

    ㅎㅇ

--- mypage/forms.py
+++ mypage/forms.py
@@ -1,7 +1,19 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput,Textarea
 from mypage.models import Article
 
 class Form(ModelForm):
     class Meta:
         model = Article
         fields = ['name', 'contents']
+        widgets = {
+            'name': TextInput(attrs={
+                'class': "name_class",
+                'style': 'max-width: 1000px;',
+                'placeholder': 'name'
+                }),
+            'contents': Textarea(attrs={
+                "class": "contents_class",
+                'placeholder': 'contents'
+                })
+            }
+        
