93522361303fc62b7b5d3cf9282d191f801aa079
imminseohoe <imminseohoe@gmail.com>
2023-11-02 04:35:29 +0700

    hasinbal

--- anniversityy/settings.py
+++ anniversityy/settings.py
@@ -2,7 +2,8 @@
 import sys
 import os
 from pathlib import Path
-
+import logging
+logging.getLogger('django.db.backends').setLevel(logging.ERROR)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
