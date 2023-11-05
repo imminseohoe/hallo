bfdd8b90035339e93fcdfd7350d62fadf2b44a92
imminseohoe <imminseohoe@gmail.com>
2023-11-06 01:51:57 +0700

    last...

--- mypage/admin.py
+++ mypage/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from . models import Article,HouseClick,ClickCount
+from . models import Article,HouseClick,ClickCount,UserProfile
 
 admin.site.register(Article)
 admin.site.register(HouseClick)
 admin.site.register(ClickCount)
+admin.site.register(UserProfile)
