cd78377631879b161b7f381bc92e4cbd0eef1e35
imminseohoe <imminseohoe@gmail.com>
2023-11-07 01:15:27 +0700

    last...

--- anniversityy/settings.py
+++ anniversityy/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-)cjjhh)(iji$8g@jf(89h0697#y*fo@3l66m=fqjiv0@)s&=q+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
