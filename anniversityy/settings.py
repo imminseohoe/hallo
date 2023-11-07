c7bac6c9ab0b8d2d446e2f70664046cf2f8ffe82
imminseohoe <imminseohoe@gmail.com>
2023-11-07 08:03:36 +0700

    True

--- anniversityy/settings.py
+++ anniversityy/settings.py
@@ -102,7 +102,7 @@ else:
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Ho_Chi_Minh'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
